853a72e8e243d084166f7995039637d1db2f0c12
srtoxyc <ivi1000vm@gmail.com>
2024-02-12 16:06:56 +0100

    v1.0.0

--- src/test/java/org/toxyc/bizum/bizum/BizumApplicationTests.java
+++ src/test/java/org/toxyc/bizum/bizum/BizumApplicationTests.java
@@ -1,13 +1,7 @@
 package org.toxyc.bizum.bizum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BizumApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
